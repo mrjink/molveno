2f751256c888c1c48a00799ab6b9b219be09114b
Gillis J. de Nijs <gillis.de-nijs@capgemini.com>
2018-11-26 17:07:08 +0100

    Add example index.html with images and styles.

--- src/main/java/com/molvenolakeresort/MolvenoLakeResortApplication.java
+++ src/main/java/com/molvenolakeresort/MolvenoLakeResortApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MolvenoLakeResortApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MolvenoLakeResortApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MolvenoLakeResortApplication.class, args);
+    }
 }
