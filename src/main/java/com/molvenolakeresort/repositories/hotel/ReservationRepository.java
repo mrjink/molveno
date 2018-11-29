3b1968bca503ad8214437c27f30d4b4f613c66fe
Yoran <yoran315@gmail.com>
2018-11-29 16:12:07 +0100

    Fixed bean name

--- src/main/java/com/molvenolakeresort/repositories/hotel/ReservationRepository.java
+++ src/main/java/com/molvenolakeresort/repositories/hotel/ReservationRepository.java
@@ -1,8 +1,9 @@
 package com.molvenolakeresort.repositories.hotel;
 
-import com.molvenolakeresort.models.hotel.Guest;
 import com.molvenolakeresort.models.hotel.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository(value = "hotelReservationRepository")
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
