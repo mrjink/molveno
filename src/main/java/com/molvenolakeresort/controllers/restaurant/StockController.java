6e9df8bf4f08adab1c2443289bba71d7d4482260
EJACOBS <bU6Am9yrRPBOSPF3aqZR>
2018-12-05 15:12:40 +0100

    StockController

--- src/main/java/com/molvenolakeresort/controllers/restaurant/StockController.java
+++ src/main/java/com/molvenolakeresort/controllers/restaurant/StockController.java
@@ -1,5 +1,48 @@
 package com.molvenolakeresort.controllers.restaurant;
 
+import com.molvenolakeresort.models.restaurant.stock.Stock;
+import com.molvenolakeresort.repositories.restaurant.stock.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/restaurant/stock")
 public class StockController {
-//    CRUD ingredient_order_list, stock, supplier, supplyorder
+
+    @Autowired
+    private final StockRepository repository;
+//
+
+    public StockController(StockRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/all")
+    List<Stock> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/add")
+    public Stock addStock(@RequestBody Stock Stock) {
+        return repository.save(Stock);
+    }
+    //
+    @GetMapping("/get/{id}")
+    public Stock getStock(@PathVariable Long id) throws EntityNotFoundException {
+
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException(id.toString()));
+    }
+    //
+    @PutMapping("/update/{id}")
+    public Stock updateStock(@RequestBody Stock updatedStock, @PathVariable Long id) {
+        return repository.save(updatedStock);
+    }
+    //
+    @DeleteMapping("/delete/{id}")
+    public void deleteStock(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
 }
